5fc17bb5d026bcf312d6a588a4feaf99c8407ebb
sriramn08 <sriram.narasimhan@yahoo.com>
2020-04-06 00:16:27 +0800

    Changes to get ccs and fs working with ribbon

---
+++ spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/LocalRibbonClientConfiguration.java
@@ -0,0 +1,18 @@
+package com.in28minutes.springboot.microservice.example.currencyconversion;
+
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ServerList;
+import org.springframework.cloud.netflix.ribbon.StaticServerList;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LocalRibbonClientConfiguration {
+    @Bean
+    public ServerList<Server> ribbonServerList() {
+        // return new ConfigurationBasedServerList();
+        StaticServerList<Server> staticServerList = new StaticServerList<>((new Server("localhost", 8001)),
+                new Server("localhost", 8000));
+        return staticServerList;
+    }
+}
