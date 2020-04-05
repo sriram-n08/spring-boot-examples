a8b2c2ae1a488f86be20d5fa38baa212feffe2cb
sriramn08 <sriram.narasimhan@yahoo.com>
2020-04-06 00:22:29 +0800

    testing git push

--- spring-boot-basic-microservice/spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/SpringBootMicroserviceForexServiceApplication.java
+++ spring-boot-basic-microservice/spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/SpringBootMicroserviceForexServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//Testing git push
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SpringBootMicroserviceForexServiceApplication {
