6975697f3f0a41251f5a6488a3fde55c2e899cc3
sriramn08 <sriram.narasimhan@yahoo.com>
2020-04-10 23:09:44 +0800

    Changes to get ccs and fs working with ribbon

--- spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/CurrencyExchangeServiceProxy.java
+++ spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/CurrencyExchangeServiceProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="forex-service")
-@RibbonClient(name="forex-service", configuration = LocalRibbonClientConfiguration.class)
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public CurrencyConversionBean retrieveExchangeValue
--- spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/LocalRibbonClientConfiguration.java
+++ spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/LocalRibbonClientConfiguration.java
@@ -6,13 +6,13 @@ import org.springframework.cloud.netflix.ribbon.StaticServerList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-class LocalRibbonClientConfiguration {
-    @Bean
-    public ServerList<Server> ribbonServerList() {
-        // return new ConfigurationBasedServerList();
-        StaticServerList<Server> staticServerList = new StaticServerList<>((new Server("localhost", 8001)),
-                new Server("localhost", 8000));
-        return staticServerList;
-    }
-}
+//@Configuration
+//class LocalRibbonClientConfiguration {
+//    @Bean
+//    public ServerList<Server> ribbonServerList() {
+//        // return new ConfigurationBasedServerList();
+//        StaticServerList<Server> staticServerList = new StaticServerList<>((new Server("localhost", 8001)),
+//                new Server("localhost", 8000));
+//        return staticServerList;
+//    }
+//}
